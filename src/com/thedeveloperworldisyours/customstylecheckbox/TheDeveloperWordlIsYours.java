355d5042ae2e705be286403f20a1406a313f02f0
javier <javier@Mac-de-javier.local>
2013-04-14 01:13:26 +0200

    first commit

---
+++ src/com/thedeveloperworldisyours/customstylecheckbox/TheDeveloperWordlIsYours.java
@@ -0,0 +1,16 @@
+package com.thedeveloperworldisyours.customstylecheckbox;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TheDeveloperWordlIsYours extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_developer_wordl_is_yours);
+    }
+
+   
+}
